ad8482ffa948435cfae8252a45b2f01719f4e34d
Ankan Giri <ankangiri05@gmail.com>
2026-05-28 19:23:46 +0530

    feat(auth): restrict login to @heritageit.edu.in and disable manual signup

--- Backend/users/serializers.py
+++ Backend/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth import get_user_model, authenticate
+from django.db.models import Q
 
 User = get_user_model()
 
@@ -15,6 +16,14 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'password', 'role', 'first_name', 'last_name')
+
+    def validate(self, attrs):
+        raise serializers.ValidationError("Account creation is being stopped for the time being until the feedback is being collected.")
+
+    def validate_email(self, value):
+        if value.lower().endswith('@heritageit.edu.in'):
+            raise serializers.ValidationError("Emails with @heritageit.edu.in domain must register/login using Google OAuth.")
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -37,6 +46,17 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         # We override validate to use our custom backend logic
         username = attrs.get(self.username_field)
         password = attrs.get('password')
+
+        if username:
+            normalized_username = username.lower()
+            is_heritage_email = normalized_username.endswith('@heritageit.edu.in')
+            is_heritage_user = User.objects.filter(
+                Q(username__iexact=username) | Q(email__iexact=username),
+                email__endswith='@heritageit.edu.in'
+            ).exists()
+
+            if is_heritage_email or is_heritage_user:
+                raise serializers.ValidationError('Accounts with @heritageit.edu.in domain must log in using Google OAuth.')
 
         user = authenticate(request=self.context.get('request'), username=username, password=password)
 
